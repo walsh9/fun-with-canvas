f069474d26e4f279f8a2ee98b6e0fb1bcf0c4dee
Matthew Walsh <walsh9@gmail.com>
2014-09-05 01:36:18 -0400

    Refactor rendering

--- raycasting/untextured-raycaster.js
+++ raycasting/untextured-raycaster.js
@@ -6,20 +6,11 @@
 (function (canvas) {
 
 var pleaseShowFps = false;
+var pleaseDoShading = true;
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 var ctx = canvas.getContext("2d");
-world = {
-    ceilColor:  "rgb( 83, 83,101)",        
-    floorColor: "rgb(121,121,174)", 
-    wallColors: [    
-    [255,255,255], // default 
-    [255,220,116], // 1
-    [153,229,104], // 2
-    [228,104,148], // 3
-    [102,102,190]  // 4
-  ],
-  map: [
+var map1 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -44,8 +35,8 @@ world = {
     [1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-  ]
-};
+  ];
+var currentMap = map1;
 var player = {
     x: 22, 
     y: 12, 
@@ -55,7 +46,107 @@ var player = {
     planeY: 0.67
 };
 
-var render = function () {
+
+var getWallColor = function (tile, side) {
+    var wallColors = [    
+        [255,255,255], // default 
+        [255,220,116], // 1
+        [153,229,104], // 2
+        [228,104,148], // 3
+        [102,102,190]  // 4
+    ];
+    var color = [];
+    if (tile > 0 && tile < wallColors.length) {
+        color = wallColors[tile].slice(0);
+    } else {
+        color = wallColors[0].slice(0);
+    }
+    if (side === 1) {
+        color[0] = parseInt(color[0] / 2);
+        color[1] = parseInt(color[1] / 2);
+        color[2] = parseInt(color[2] / 2);
+    }
+    return color;
+};
+
+var shadeColor = function(color, distance) {
+    var brightness = Math.min(1, 1/distance * 6);
+    color[0] = parseInt(color[0] * brightness);
+    color[1] = parseInt(color[1] * brightness);
+    color[2] = parseInt(color[2] * brightness);
+    return color;
+};
+
+var drawCeiling = function(ctx) {
+    var color = "rgb( 83, 83,101)";
+    ctx.fillStyle = color; 
+    ctx.fillRect(0.5,0.5,canvasWidth,canvasHeight);
+};
+
+var drawFloor = function(ctx) {
+    var color = "rgb(121,121,174)";
+    ctx.fillStyle = color; 
+    ctx.fillRect(0.5,canvasHeight / 2 - 0.5,canvasWidth,canvasHeight);
+};
+
+var drawWalls = function (ctx, viewer, map) {
+    var perpWallDist, lineHeight, drawStart, drawEnd, colorRGB, oldColor, color;
+    ctx.beginPath();
+    for (var x = 0.5; x < canvasWidth; x++) {
+        var cameraX = 2 * x / canvasWidth - 1,
+            rayPosX = viewer.x,
+            rayPosY = viewer.y,
+            rayDirX = viewer.dirX + viewer.planeX * cameraX,
+            rayDirY = viewer.dirY + viewer.planeY * cameraX,
+            mapX = parseInt(rayPosX),
+            mapY = parseInt(rayPosY),
+            deltaDistX = Math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX)),
+            deltaDistY = Math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY)),
+            hit = 0,
+            stepX = (rayDirX < 0) ? -1 : 1,
+            stepY = (rayDirY < 0) ? -1 : 1,
+            sideDistX = (rayDirX < 0) ? (rayPosX - mapX) * deltaDistX : (mapX + 1 - rayPosX) * deltaDistX,
+            sideDistY = (rayDirY < 0) ? (rayPosY - mapY) * deltaDistY : (mapY + 1 - rayPosY) * deltaDistY;
+        while (hit === 0) {
+            if (sideDistX < sideDistY) {
+                sideDistX += deltaDistX;
+                mapX += stepX;
+                side = 0;
+            } else {
+                sideDistY += deltaDistY;
+                mapY += stepY;
+                side = 1;
+            }
+            if (map[mapX][mapY] > 0) hit = 1;
+        }
+        perpWallDist = (side === 0) ?
+            Math.abs((mapX - rayPosX + (1 - stepX) / 2) / rayDirX) 
+            :
+            Math.abs((mapY - rayPosY + (1 - stepY) / 2) / rayDirY);
+        lineHeight = Math.abs(parseInt(canvasHeight / perpWallDist));
+        drawStart = Math.max(0, -lineHeight / 2 + canvasHeight / 2);
+        drawEnd = Math.min(canvasHeight - 1, lineHeight / 2 + canvasHeight / 2);
+        colorRGB = getWallColor(map[mapX][mapY], side);
+        if (pleaseDoShading) {
+            colorRGB = shadeColor(colorRGB, perpWallDist);
+        }
+        color = "rgb(" + colorRGB.join(",") + ")";
+        if (oldColor && color !== oldColor) {
+            ctx.strokeStyle = oldColor;
+            ctx.stroke();
+            ctx.closePath();
+            ctx.beginPath();
+        }
+        ctx.moveTo(x, drawStart);
+        ctx.lineTo(x, drawEnd);
+        oldColor = color;
+    }
+    ctx.strokeStyle = color; 
+    ctx.stroke();
+    ctx.closePath();
+};
+
+var render = function (ctx, viewer, map) {
     var cameraX,
         rayPosX, rayPosY,
         rayDirX, rayDirY,
@@ -71,77 +162,9 @@ var render = function () {
     ctx.save();
     ctx.lineCaps = "square";
     ctx.clearRect(0,0,canvasWidth,canvasHeight);
-    ctx.fillStyle = world.ceilColor; //ceiling
-    ctx.fillRect(0.5,0.5,canvasWidth,canvasHeight);
-    ctx.fillStyle = world.floorColor; //floor
-    ctx.fillRect(0.5,canvasHeight / 2 - 0.5,canvasWidth,canvasHeight);
-    ctx.beginPath();
-    for (var x = 0.5; x < canvasWidth; x++) {
-        cameraX = 2 * x / canvasWidth - 1;
-        rayPosX = player.x;
-        rayPosY = player.y;
-        rayDirX = player.dirX + player.planeX * cameraX;
-        rayDirY = player.dirY + player.planeY * cameraX;
-        mapX = parseInt(rayPosX);
-        mapY = parseInt(rayPosY);
-        deltaDistX = Math.sqrt(1 + (rayDirY * rayDirY) / (rayDirX * rayDirX));
-        deltaDistY = Math.sqrt(1 + (rayDirX * rayDirX) / (rayDirY * rayDirY));
-        hit = 0;
-        stepX = (rayDirX < 0) ? -1 : 1;
-        stepY = (rayDirY < 0) ? -1 : 1;
-        sideDistX = (rayDirX < 0) ? (rayPosX - mapX) * deltaDistX : (mapX + 1 - rayPosX) * deltaDistX;
-        sideDistY = (rayDirY < 0) ? (rayPosY - mapY) * deltaDistY : (mapY + 1 - rayPosY) * deltaDistY;
-        while (hit === 0) {
-            if (sideDistX < sideDistY)
-            {
-                sideDistX += deltaDistX;
-                mapX += stepX;
-                side = 0;
-            }
-            else
-            {
-                sideDistY += deltaDistY;
-                mapY += stepY;
-                side = 1;
-            }
-            if (world.map[mapX][mapY] > 0) hit = 1;
-        }
-        perpWallDist = (side === 0) ?
-            Math.abs((mapX - rayPosX + (1 - stepX) / 2) / rayDirX) 
-            :
-            Math.abs((mapY - rayPosY + (1 - stepY) / 2) / rayDirY);
-        lineHeight = Math.abs(parseInt(canvasHeight / perpWallDist));
-        drawStart = Math.max(0, -lineHeight / 2 + canvasHeight / 2);
-        drawEnd = Math.min(canvasHeight - 1, lineHeight / 2 + canvasHeight / 2);
-        mapTile = world.map[mapX][mapY];
-        if (mapTile > 0 && mapTile < world.wallColors.length) {
-            color = world.wallColors[mapTile].slice(0);
-        } else {
-            color = world.wallColors[0].slice(0);
-        }
-        if (side === 1) {
-            color[0] = parseInt(color[0] / 2);
-            color[1] = parseInt(color[1] / 2);
-            color[2] = parseInt(color[2] / 2);
-        }
-        brightness = Math.min(1, 1/perpWallDist * 6);
-        color[0] = parseInt(color[0] * brightness);
-        color[1] = parseInt(color[1] * brightness);
-        color[2] = parseInt(color[2] * brightness);
-        color = "rgb(" + color.join(",") + ")";
-        if (oldColor && color !== oldColor) {
-            ctx.strokeStyle = oldColor;
-            ctx.stroke();
-            ctx.closePath();
-            ctx.beginPath();
-        }
-        ctx.moveTo(x, drawStart);
-        ctx.lineTo(x, drawEnd);
-        oldColor = color;
-    }
-    ctx.strokeStyle = color; 
-    ctx.stroke();
-    ctx.closePath();
+    drawCeiling(ctx);
+    drawFloor(ctx);
+    drawWalls(ctx, viewer, map);
     ctx.restore();
 };
 
@@ -194,9 +217,6 @@ actions = {
         oldPlaneX = actor.planeX;
         actor.planeX = actor.planeX * Math.cos(rotSpeed) - actor.planeY * Math.sin(rotSpeed);
         actor.planeY = oldPlaneX * Math.sin(rotSpeed) + actor.planeY * Math.cos(rotSpeed);
-    },
-    toggle: function(option) {
-        option = !option;
     }
 };
 
@@ -206,10 +226,10 @@ var update = function(modifier) {
     var rotSpeed =  modifier * 3;
     var oldDirX, oldPlaneX;
     if (38 in keysDown) { // up
-        actions.moveForward(player, world.map, moveSpeed);
+        actions.moveForward(player, currentMap, moveSpeed);
     }
     if (40 in keysDown) { // down
-        actions.moveBackward(player, world.map, moveSpeed);
+        actions.moveBackward(player, currentMap, moveSpeed);
     }
     if (39 in keysDown) { // right
         actions.turnRight(player, rotSpeed);
@@ -217,9 +237,13 @@ var update = function(modifier) {
     if (37 in keysDown) { // left
         actions.turnLeft(player, rotSpeed);
     }
-    if (70 in keysDown) { // left
-      pleaseShowFps = ! pleaseShowFps;
+    if (70 in keysDown) { // f
+      pleaseShowFps = !pleaseShowFps;
       delete keysDown[70];
+    }
+    if (83 in keysDown) { // s
+      pleaseDoShading = !pleaseDoShading;
+      delete keysDown[83];
     }
 };
 
@@ -235,12 +259,12 @@ var main = function () {
     var fps = (1000/delta);
 
     update(delta / 1000);
-    render();
+    render(ctx, player, currentMap);
     if (pleaseShowFps) {
         showFps(ctx, fps);
     }
     then = now;
-    requestAnimationFrame(main);
+    requestAnimationFrame(main, canvas);
 };
 var then = Date.now();
 main();

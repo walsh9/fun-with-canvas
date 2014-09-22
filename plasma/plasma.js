21cf1c729d56345d8ce359588cd93ff3f127dcb6
Matthew Walsh <walsh9@gmail.com>
2014-09-21 23:19:02 -0400

    Cleanup. Make palette shift time-based, independent of rendering speed.

--- plasma/plasma.js
+++ plasma/plasma.js
@@ -4,11 +4,16 @@
  */
 
 (function (canvas) {
-var w = canvas.width;
-var h = canvas.height;
-var ctx = canvas.getContext("2d");
+    'use strict';
+    var w = canvas.width;
+    var h = canvas.height;
+    var ctx = canvas.getContext("2d");
+    // var keysDown = {};
+    var mouseX = 0;
+    var mouseY = 0;
+    var paletteOffset = 0;
 
-var render = function () {
+    /*jslint unparam: true*/
     var plasmaFn = [
         function (x, y, s) {
             return 128 + (128 * Math.sin(x / s));
@@ -21,8 +26,8 @@ var render = function () {
         },
         function (x, y, s) {
             return (128 + (128 * Math.sin(
-                Math.sqrt(((x - w / 2) * (x - w / 2) + (y - h / 2) * (y - h / 2))) / 
-                s)));
+                Math.sqrt(((x - w / 2) * (x - w / 2) + (y - h / 2) * (y - h / 2))) / s
+            )));
         },
         function (x, y, s) {
             return 128 + (128 * Math.sin(Math.sqrt((x * x + y * y)) / s));
@@ -32,98 +37,88 @@ var render = function () {
         },
         function (x, y, s) {
             return (128 + (128 * Math.sin(
-                Math.sqrt(((x - w / 2) * (x - w / 2) + (y - h / 2) * (y - h / 2))) / 
-                s)));
+                Math.sqrt(((x - w / 2) * (x - w / 2) + (y - h / 2) * (y - h / 2))) / s
+            )));
         },
     ];
-    var imgData = ctx.createImageData(w, h);
-    var palette = [];
-    var rx = Math.pow(2, parseInt(Math.random() * 9)); //factors of 256 will loop nice
-    var gx = Math.pow(2, parseInt(Math.random() * 9));
-    var bx = Math.pow(2, parseInt(Math.random() * 9));
-    var s = parseInt(Math.random() * 120 + 8);
-    for(var i = 0; i < 256; i++) {
-        var r = parseInt(128 + 128 * Math.sin(3.1415 * i / rx ));
-        var g = parseInt(128 + 128 * Math.sin(3.1415 * i / gx ));
-        var b = parseInt(128 + 128 * Math.sin(3.1415 * i / bx ));
-        palette.push([r,g,b]);
-    }
-    var paletteOffset = 0;
-    return function () {
-        plasmas = [plasmaFn[0], plasmaFn[1], plasmaFn[5]];
-        for(var x = 0; x < w; x++) {
-            for(var y = 0; y < h; y++) {
-                var c = 0;
-                for (var p = 0; p < plasmas.length; p++) {
-                          c += plasmas[p](x, y, s);
-                }
-                c = parseInt(c / plasmas.length);
-                color = palette[(c + paletteOffset) % 255];
-                var offset = (x + y * w) * 4;
-                imgData.data[offset + 0] = color[0];
-                imgData.data[offset + 1] = color[1];
-                imgData.data[offset + 2] = color[2];
-                imgData.data[offset + 3] = 255; //opaque
+    /*jslint unparam: false*/
+
+    var render = function () {
+        var imgData = ctx.createImageData(w, h);
+        var palette = [];
+        var rx = Math.pow(2, Math.floor(Math.random() * 9)); //factors of 256 will loop nice
+        var gx = Math.pow(2, Math.floor(Math.random() * 9));
+        var bx = Math.pow(2, Math.floor(Math.random() * 9));
+        var s = Math.floor(Math.random() * 120 + 8);
+
+        var i, r, g, b, plasmas;
+        for (i = 0; i < 256; i++) {
+            r = Math.floor(128 + 128 * Math.sin(3.1415 * i / rx));
+            g = Math.floor(128 + 128 * Math.sin(3.1415 * i / gx));
+            b = Math.floor(128 + 128 * Math.sin(3.1415 * i / bx));
+            palette.push([r, g, b]);
         }
-                  }
-                  paletteOffset++; 
-        ctx.putImageData(imgData, 0, 0);
+        return function () {
+            var x, y, c,
+                p, color, offset;
+            plasmas = [plasmaFn[0], plasmaFn[1], plasmaFn[5]];
+            for (x = 0; x < w; x++) {
+                for (y = 0; y < h; y++) {
+                    c = 0;
+                    for (p = 0; p < plasmas.length; p++) {
+                        c += plasmas[p](x, y, s);
+                    }
+                    c = Math.floor(c / plasmas.length);
+                    color = palette[(c + Math.floor(paletteOffset)) % 255];
+                    offset = (x + y * w) * 4;
+                    imgData.data[offset + 0] = color[0];
+                    imgData.data[offset + 1] = color[1];
+                    imgData.data[offset + 2] = color[2];
+                    imgData.data[offset + 3] = 255; //opaque
+                }
+            }
+            ctx.putImageData(imgData, 0, 0);
+        };
     };
-};
+    var draw = render();
 
-var requestAnimationFrame = window.requestAnimationFrame || 
-    window.webkitRequestAnimationFrame || 
-    window.msRequestAnimationFrame || 
-    window.mozRequestAnimationFrame || 
-    window.oRequestAnimationFrame;
+    var requestAnimationFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame;
 
-var keysDown = {};
-var mouseX = 0;
-var mouseY = 0;
-
-addEventListener("keydown", function (e) {
-    keysDown[e.keyCode] = true;
-}, false);
-
-addEventListener("keyup", function (e) {
-    delete keysDown[e.keyCode];
-}, false);
-
-canvas.addEventListener("mousemove", function (e) {
+    canvas.addEventListener("mousemove", function (e) {
         e = e || window.event; // IE-ism
         mouseX = e.clientX;
         mouseY = e.clientY;
-}, false);
+    }, false);
 
-canvas.addEventListener("click", function (e) {
-    draw = render();
-}, false);
+    canvas.addEventListener("click", function (e) {
+        draw = render();
+    }, false);
 
-var update = function(modifier) {
-    if (38 in keysDown) { // up
+    var timeStep = (1 / 60) * 1000;
+    var update = function (delta) {
+        var step = (timeStep / delta);
+        paletteOffset += step;
+        console.log(Math.floor(paletteOffset) % 255);
+    };
 
-    }
-    if (40 in keysDown) { // down
+    var currentTime = Date.now();
+    var main = function () {
+        var newTime = Date.now();
+        var frameTime = newTime - currentTime;
+        var delta;
+        currentTime = newTime;
+        while (frameTime > 0) {
+            delta = Math.min(frameTime, timeStep);
+            update(delta);
+            frameTime -= delta;
+        }
+        draw();
+        requestAnimationFrame(main);
+    };
 
-    }
-    if (39 in keysDown) { // right
-
-    }
-    if (37 in keysDown) { // left
-
-    }
-};
-var draw = render();
-
-var main = function () {
-    var now = Date.now();
-    var delta = now - then;
-    update(delta / 1000);
-    draw();
-    then = now;
-    requestAnimationFrame(main);
-};
-var then = Date.now();
-main();
-
+    main();
 }(canvas));
